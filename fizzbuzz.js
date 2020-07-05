2f362552816a6329167d342fd968c52d488c0366
takuya tamada <tmdtky@gmail.com>
2020-07-05 20:17:55 +0900

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+    console.log(i);
+}
